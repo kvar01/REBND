416c08816fd4aefd8d6788b2889e2fc22f7223c8
kvar01 <73667139+kvar01@users.noreply.github.com>
2023-05-06 17:10:52 -0400

    Update ClientHeaderTitle.php

--- ClientHeaderTitle.php
+++ ClientHeaderTitle.php
@@ -1,7 +1,6 @@
 <title>Rebnd Shop</title>
 <link rel="icon" href="images/log2.png" />
     <link rel="stylesheet" href="fonts/icofont/icofont.min.css" />
-    <link rel="stylesheet" href="fonts/flaticon/grocery/flaticon.css" />
     <link rel="stylesheet" href="css/vendor/slick.css" />
     <link rel="stylesheet" href="css/vendor/bootstrap.css" />
     <link rel="stylesheet" href="css/custom/main.css" />
